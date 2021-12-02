27aef0c2efe19d924902399527d67b35acc250d9
TadeuszSikorski <sikorski.tadeusz@yahoo.com>
2021-12-02 12:27:12 +0100

    correcting solution of day 01

--- 2021/01/solution.py
+++ 2021/01/solution.py
@@ -19,14 +19,18 @@ def count_number_of_times_depth_measurement_increases(data):
     return counter
 
 
+def _count_sum_of_sliding_window(data, indices):
+    return data[indices[0]] + data[indices[1]] + data[indices[2]]
+
+
 def count_number_of_times_sum_of_measurements_in_sliding_window_increases(data):
     index, counter = 0, 0
 
     while index + 3 < len(data):
-        first_sliding_window = data[index] + data[index + 1] + data[index + 2]
-        second_sliding_window = data[index + 1] + data[index + 2] + data[index + 3]
+        first_indices = [index, index + 1, index + 2]
+        second_indices = [index + 1, index + 2, index + 3]
  
-        if first_sliding_window < second_sliding_window:
+        if _count_sum_of_sliding_window(data, first_indices) < _count_sum_of_sliding_window(data, second_indices):
             counter += 1
 
         index += 1
