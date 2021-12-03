ab5e42f70414942161ee49d861949c8b8552c96b
TadeuszSikorski <sikorski.tadeusz@yahoo.com>
2021-12-03 12:15:38 +0100

    update solution for day 02 and add test data

--- 2021/02/solution.py
+++ 2021/02/solution.py
@@ -1,9 +1,9 @@
 def transform_data(data):
     transformed_data = []
-    
+
     for line in data:
         direction, number = line.split()
-      
+
         transformed_data.append(tuple([direction, int(number)]))
 
     return transformed_data
@@ -15,12 +15,12 @@ def multiply_horizontal_position_and_depth(data):
     for line in data:
         if "forward" == line[0]:
             horizontal_position += line[1]
-        
+
         if "down" == line[0]:
             depth += line[1]
         if "up" == line[0]:
             depth -= line[1]
-    
+
     return horizontal_position * depth
 
 
@@ -32,10 +32,10 @@ def multiply_horizontal_position_and_depth_from_new_rules(data):
         if "forward" == line[0]:
             horizontal_position += line[1]
             depth += aim * line[1]
-        
+
         if "down" == line[0]:
             aim += line[1]
         if "up" == line[0]:
             aim -= line[1]
-    
+
     return horizontal_position * depth
